0b50831bf3151d871c92c25ecc412d14f1a7e1b0
chrisS41 <swseok94@gmail.com>
2021-08-12 11:11:16 +0900

    copy command line arg

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
-
+import java.util.Scanner;
 
 
 
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//Scanner sc = new Scanner(System.in);
+		//String scanned = sc.next();
+		//System.out.println(sc.next());
+
+		System.out.println(args[0]);
 	}
 }
